0fbf195f211a7dc976ac8c6b38e8b70f348f9917
nomad-coder420 <saketmahajan0@gmail.com>
2025-07-19 20:15:10 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useNova, useNovaObject } from "nova-react-sdk";
 
-function App() {
+const MyComponent = () => {
+  const { props: testButtonProps } = useNovaObject("test-object-1");
+
+  return (
+    <button style={{ backgroundColor: testButtonProps?.color }}>
+      Test Button
+    </button>
+  );
+};
+
+const App = () => {
+  const { setUser, loadAllObjects, trackEvent, state } = useNova();
+
+  const [novaLoaded, setNovaLoaded] = useState(false);
+
+  useEffect(() => {
+    const appInit = async () => {
+      await setUser({
+        userId: "user78211qaqe2e1a1132",
+        userProfile: { utm_source: "facebook" },
+      });
+    };
+
+    appInit();
+  }, []);
+
+  useEffect(() => {
+    const loadNovaObjects = async () => {
+      await loadAllObjects();
+      setNovaLoaded(true);
+    };
+
+    if (state.user) {
+      loadNovaObjects();
+      trackEvent("Test Event", {
+        test: "test",
+      });
+    }
+  }, [state.user]);
+
+  console.log(state.objects);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +60,10 @@ function App() {
         >
           Learn React
         </a>
+        {novaLoaded && <MyComponent />}
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { NovaProvider } from "nova-react-sdk";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <NovaProvider
+      config={{
+        organisationId: "org123",
+        appId: "app123",
+        apiEndpoint: "http://127.0.0.1:8000",
+      }}
+    >
+      <App />
+    </NovaProvider>
   </React.StrictMode>
 );
 
